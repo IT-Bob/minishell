818401f040d8712a9d43256fe05d5af82763de95
Alexis Guérin <aguerin42@icloud.com>
2017-08-13 18:22:00 +0200

    Lancement d'exécutable possible avec / ou ./

--- src/executables/exec.c
+++ src/executables/exec.c
@@ -7,6 +7,67 @@
 */
 
 #include "minishell.h"
+#include "ft_printf.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+/**
+** \brief	Recherche de variable d'environnement.
+**
+** \param	Variable à rechercher.
+** \param	Environnement dans lequel chercher la variable.
+**
+** \return	Indice de la variable dans l'environnement ou -1 si celle-ci n'y
+**			est pas.
+*/
+static int	find_var(char *var, char **environnement)
+{
+	int	i;
+	int	len;
+
+	if (var && environnement)
+	{
+		i = -1;
+		len = ft_strlen(var);
+		while (environnement[++i])
+			if (ft_strnequ(var, environnement[i], len))
+				if ((environnement[i][len] && environnement[i][len] == '='))
+					return (i);
+	}
+	return (-1);
+}
+
+/*
+** Cherche l'exécutable dans le PATH de l'environnement.
+*/
+static int	search_exec(char **argv, char ***old_env, char ***new_env)
+{
+	int		i;
+	char	*tmp;
+	char	*cut;
+	char	*path;
+
+	if (argv && old_env && new_env)
+	{
+		i = find_var("PATH", old_env[0]);
+		path = ((i >= 0) ? old_env[0][i] : NULL);
+		if (path && (tmp = ft_strchr(path, '=')))
+			path = tmp + 1;
+		tmp = NULL;
+		if (path)
+		{
+			cut = ft_strchr(path, ':');
+			tmp = (cut ? ft_strsub(path, 0, cut - path) : path);
+			ft_putendl(tmp);
+			return (0);
+		}
+		else
+			return (ft_printf("minishell : Commande inconnue : %s\n", argv[0]));
+	}
+	return (1);
+}
 
 /**
 ** \brief	Lancement d'exécutables.
@@ -26,11 +87,21 @@
 */
 int	exec(char **argv, char ***old_env, char ***new_env)
 {
-	ft_putendl("EXEC");
-	if (argv && old_env)
+	pid_t	f;
+
+	if (argv && argv[0] && old_env)
 	{
 		if (!new_env)
 			new_env = old_env;
+		if ((f = fork()))
+			wait(NULL);
+		else
+		{
+			if (argv[0][0] && (argv[0][0] == '/' || ft_strnequ(argv[0], "./", 2)))
+				execve(argv[0], argv, new_env[0]);
+			else
+				return (search_exec(argv, old_env, new_env));
+		}
 	}
 	return (1);
 }
