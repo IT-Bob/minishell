fd0c534b4be1ca57b551cff4708d37b95d8e2d05
Alexis Guerin <aguerin@student.42.fr>
2017-08-23 12:17:20 +0200

    Changement de libft.c en gen.c

---
+++ src/gen.c
@@ -0,0 +1,59 @@
+/**
+** \file	gen.c
+** \author	Alexis Guérin
+** \date	août 2017
+**
+** \brief	Fonctions générales ou à déplacer dans libft.
+*/
+
+#include "minishell.h"
+
+/**
+** \brief	Affiche l'environnement.
+**
+** \param	environnement -	Environnement à afficher.
+*/
+
+void	print_env(char **environnement)
+{
+	ft_strdoubleiter(environnement, (void*)&ft_putendl);
+}
+
+/**
+** \brief	Calcul la longueur de la première dimension d'un double pointeur
+**			sur char.
+**
+** \param	doublestr -	Double pointeur sur char.
+**
+** \return	Longueur de la première dimension.
+*/
+
+int		ft_strlendouble(char **doublestr)
+{
+	int	i;
+
+	i = 0;
+	if (doublestr)
+		while (doublestr[i])
+			i++;
+	return (i);
+}
+
+/**
+** \brief	Applique une fonction à chaque chaîne d'un double pointeur sur char.
+**
+** \param	doublestr -	Double chaîne sur laquelle appliquer la fonction.
+** \param	f -			Fonction à appliquer.
+*/
+
+void	ft_strdoubleiter(char **doublestr, void (*f)(char *str))
+{
+	int	i;
+
+	if (doublestr && f)
+	{
+		i = 0;
+		while (doublestr[i])
+			f(doublestr[i++]);
+	}
+}
